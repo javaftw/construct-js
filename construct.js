b8eafe25b0cb43f95d8ba72f35625794e0298469
Tarun Boddupalli <tarunbod@gmail.com>
2016-02-16 18:34:39 -0500

    update

--- construct.js
+++ construct.js
@@ -1,18 +1,35 @@
-var Construct = (function construct() {
+'use strict';
+
+var Construct = (function() {
 
 	// TODO: add width, height, and other animatable properties
-	var Construct = {
+	var _Construct = {
 		KeyValueType: {
-			OPACITY: 0,
-			TRANSLATE: 1,
-			ROTATE: 2,
-			SCALE: 3
+			OPACITY: {
+				isTransform: false,
+				id: 0
+			},
+			TRANSLATE: {
+				isTransform: true,
+				id: 1
+			},
+			ROTATE: {
+				isTransform: true,
+				id: 2
+			},
+			SCALE: {
+				isTransform: true,
+				id: 3
+			}
 		}
 	};
 
+    var TAB_1 = '    ';
+    var TAB_2 = '        ';
+
 	var Animation, KeyFrame, KeyValue;
 
-	Construct.Animation = Animation = function(options) {
+	_Construct.Animation = Animation = function(options) {
 		options = options || {};
 		this.element = options.element;
 		this.name = options.name;
@@ -30,85 +47,102 @@ var Construct = (function construct() {
 	};
 
 	Animation.prototype.export = function() {
-		var keyFramesString = this.name + "{\n";
+		var keyFramesString = this.name + " {\n";
 		var duration = this.duration;
 		for (var i = 0; i < this.keyFrames.length; i++) {
 			var currentKeyFrame = this.keyFrames[i];
 			var percentage = Math.round((currentKeyFrame.time / duration) * 100);
-			keyFramesString += percentage + '% {\n';
+			keyFramesString += TAB_1 + percentage + '% {\n';
+            if (currentKeyFrame.hasTransforms) {
+                keyFramesString += 
+                                TAB_2 + currentKeyFrame.endTransformValue + ';\n' + 
+                                TAB_2 + '-webkit-' + currentKeyFrame.endTransformValue + ';';
+            }
 			for (var j = 0; j < currentKeyFrame.values.length; j++) {
-
+                for (var k = 0; k < currentKeyFrame.values.length; k++) {
+                    keyFramesString += TAB_2 + currentKeyFrame.values[i].value + ';';
+                }
 			}
+            keyFramesString += '\n' + TAB_1 + '}\n';
 		}
+        keyFramesString += '}\n';
+        $('body').append('<style> @keyframes ' + keyFramesString + '\n\n@-webkit-keyframes ' + keyFramesString + '\n</style>');
 	};
 
 	Animation.prototype.start = function() {
 		$("body").append(
 			"<style>\n" +
-			this.element + ' { ' 
-				'animation-name: ' + this.name + ';\n' +
-				'animation-duration: ' + this.duration + 'ms;\n' + 
-				'animation-timing-function: ' + this.timingFunction + ';\n' + 
-				'animation-delay: ' + this.delay + ';\n' + 
-				'animation-iteration-count: ' + this.iterationCount + ';\n' + 
-				'animation-direction: ' + this.direction + ';\n' + 
-				'animation-fill-mode: ' + this.fillMode + ';\n' + 
+			this.element + ' {\n ' +
+			'    animation-name: ' + this.name + ';\n' +
+			'    animation-duration: ' + this.duration + 'ms;\n' + 
+			'    animation-timing-function: ' + this.timingFunction + ';\n' + 
+			'    animation-delay: ' + this.delay + ';\n' + 
+			'    animation-iteration-count: ' + this.iterationCount + ';\n' + 
+			'    animation-direction: ' + this.direction + ';\n' + 
+			'    animation-fill-mode: ' + this.fillMode + ';\n' + 
 			"}\n" +
 			"</style>"
 		);
 	};
 
-	Construct.KeyFrame = KeyFrame = function(time, values) {
+	_Construct.KeyFrame = KeyFrame = function(time, values) {
 		this.time = time;
-		var valueIndices = [];
+		var transformCount = 0, valueIndices = [], transformValues = [];
 		for (var i = 0; i < values.length; i++) {
-			// TODO: check for multiple transforming values, put them into one value
+			if (values[i].valueType.isTransform) {
+				transformCount++;
+				valueIndices.push(i);
+			}
 		}
+
+		for (var i = valueIndices.length - 1; i >= 0; i--) {
+			transformValues.push(values.splice(valueIndices[i], 1)[0]);
+		}
+
+		for (var i = 0; i < transformValues.length; i++) {
+			transformValues[i] = transformValues[i].simpleValue;
+		}
+
+        this.values = values;
+		this.endTransformValue = 'transform: ' + transformValues.join(" ");
+		this.hasTransforms = transformCount != 0;
 	};
 
-	Construct.KeyValue = KeyValue = function(valueType, value) {
-		this.isTransform = valueType != 0;
-		if (valueType == Construct.KeyValueType.OPACITY) {
-			this.value = 'opacity: ';
+	_Construct.KeyValue = KeyValue = function(valueType, value) {
+		this.valueType = valueType;
+		this.simpleValue = '';
+
+		if (valueType.id == _Construct.KeyValueType.OPACITY.id) {
 			if (!Array.isArray(value)) {
-				this.value += value;
-			} else {
-				this.value = 'opacity: ' + value[0];
+				this.value = 'opacity: ' + value;
 			}
 		} else { 
 			this.value = 'transform: ';
-			if (valueType == Construct.KeyValueType.TRANSLATE) {
+			if (valueType.id == _Construct.KeyValueType.TRANSLATE.id) {
 				if (Array.isArray(value)) {
 					if (value.length == 2) {
-						this.value += 'translate(' + value[0] + 'px, ' + value[1] + 'px)';
+						this.value += this.simpleValue = 'translate(' + value[0] + 'px, ' + value[1] + 'px)';
 					} else if (value.length == 3) {
-						this.value += 'translate3d(' + value[0] + 'px,' + value[1] + 'px, ' + value[2] + 'px)';
+						this.value += this.simpleValue = 'translate3d(' + value[0] + 'px, ' + value[1] + 'px, ' + value[2] + 'px)';
 					}
 				}
-			} else if (valueType == Construct.KeyValueType.ROTATE) {
+			} else if (valueType.id == _Construct.KeyValueType.ROTATE.id) {
 				if (Array.isArray(value)) {
 					if (value.length == 3) {
-						this.value += 'rotateX(' + value[0] + 'deg), rotateY(' + value[1] + 'deg), rotateZ(' + value[2] + 'deg)';
+						this.value += this.simpleValue = 'rotateX(' + value[0] + 'deg) rotateY(' + value[1] + 'deg) rotateZ(' + value[2] + 'deg)';
 					}
 				} else {
-					this.value += 'rotate(' + value + 'deg)';
+					this.value += this.simpleValue = 'rotate(' + value + 'deg)';
 				}
-			} else if (valueType == Construct.KeyValueType.SCALE) {
+			} else if (valueType.id == _Construct.KeyValueType.SCALE.id) {
 				if (Array.isArray(value)) {
 					if (value.length == 2) {
-						this.value += 'scale(' + value[0] + ', ' + value[1] + ')';
+						this.value += this.simpleValue = 'scale(' + value[0] + ', ' + value[1] + ')';
 					}
 				}
 			}
 		}
 	};
 
-
-
-	this.KeyFrame = function(options) {
-		this.value = options.value;
-		this.time = options.time;
-	}
-
-	return Construct;
+	return _Construct;
 })();
